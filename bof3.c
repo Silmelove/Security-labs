f731a2351923f4909fd77ff8ffbce221372a50c7
Neodev <132383943+Silmelove@users.noreply.github.com>
2024-09-21 19:52:40 +0700

    Add files via upload

---
+++ bof3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+void shell() {
+    printf("You made it! The shell() function is executed\n");
+}
+
+void sup() {
+    printf("Congrat!\n");
+}
+
+void main()
+{ 
+    int var;
+    void (*func)()=sup;
+    char buf[128];
+    fgets(buf,133,stdin);
+    func();
+}
+
